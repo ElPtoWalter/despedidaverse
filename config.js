8e446258989fb6f8784afa92bb89aafab84d31f8
ElPtoWalter <eduardo.efernandez.rodriguez@gmail.com>
2026-08-05 17:50:54 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 window.DV_CONFIG = Object.freeze({
   companyName: 'DespedidaVerse Studio',
   contactEmail: 'fdez.edu00@gmail.com',
-  siteUrl: 'https://elptowalter.github.io/despedidaverse-studio/',
+  siteUrl: 'https://despedidaverse.com/',
 
-  // Pega aquí la URL del despliegue de Google Apps Script cuando lo actives.
-  // Mientras esté vacía, el formulario seguirá preparando un correo normal.
-  appsScriptUrl: '',
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxIqb1g2-7QK3lugDkGcfQWU2o3r2c-qSp-om2cdNa_4s6enhqTS457SIuI1xL_kno0HQ/exec',
 
-  // Opcional: formulario completo de Tally. Ejemplo: https://tally.so/r/xxxxxx
   tallyFormUrl: '',
-
-  // Opcional: número internacional sin espacios, por ejemplo 34600111222.
   whatsappNumber: '',
 
-  // Opcional: enlaces de reserva creados en Stripe Payment Links.
   paymentLinks: {
     esencial: '',
     universo: '',
